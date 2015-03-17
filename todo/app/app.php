52511e009ba17a7cf83800fc8e550a7e60ddf659
tanner heffner & reid baldwin <student@epicodus.com>
2015-03-17 13:29:49 -0700

    fix individual list functionality

--- todo/app/app.php
+++ todo/app/app.php
@@ -25,10 +25,9 @@
         return $app['twig']->render('tasks.twig', array('tasks' => Task::getAll()));
     });
 
-
-    $app->get("/categories", function() use ($app) {
-        return $app['twig']->render('categories.twig', array('categories' => Category::getAll()));
-    });
+    // $app->get("/categories", function() use ($app) {
+    //     return $app['twig']->render('categories.twig', array('categories' => Category::getAll()));
+    // });
 
     $app->post("/tasks", function() use ($app) {
         $description = $_POST['description'];
@@ -36,12 +35,12 @@
         $task = new Task($description, $id = null, $category_id);
         $task->save();
         $category = Category::find($category_id);
-        return $app['twig']->render('category.twig', array('category' => $category, 'tasks' => Task::getAll()));
+        return $app['twig']->render('category.twig', array('category' => $category, 'tasks' => $category->getTasks()));
     });
 
     $app->post("/delete_tasks", function() use ($app) {
         Task::deleteAll();
-        return $app['twig']->render('index.twig');
+        return $app['twig']->render('index.twig', array('categories' => Category::getAll()));
     });
 
     $app->post("/categories", function() use ($app) {
